5abfb768fc8bbac43159c32d32d353c346850ba0
anshuman morey <anshumanmorey2@gmail.com>
2025-10-04 15:09:11 +0530

    array students added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,19 @@
 import "./Home.css"
 
-function Home(){
-    return(
+const students = [{ name: "anshuman", age: 20, gender: "male", city: "pune", id: 1 },
+{ name: "ankit", age: 22, gender: "male", city: "mumbai", id: 2 },
+{ name: "priyanka", age: 23, gender: "female", city: "delhi", id: 3 },
+{ name: "sonali", age: 21, gender: "male", city: "bangalore", id: 4 },
+{ name: "neha", age: 20, gender: "female", city: "chennai", id: 5 },
+{ name: "rahul", age: 24, gender: "male", city: "kolkata", id: 6 },
+{ name: "sneha", age: 22, gender: "female", city: "pune", id: 7 },
+{ name: "manish", age: 23, gender: "male", city: "mumbai", id: 8 },
+{ name: "tina", age: 21, gender: "female", city: "delhi", id: 9 },
+{ name: "raj  ", age: 20, gender: "male", city: "bangalore", id: 10 }];
+function Home() {
+    return (
         <div>
-            hello
+
         </div>
     )
 }
